d60d4d9ecfd35a29836c58653058836e7c5b6707
Krati Mittal <krati@nickelfox.co>
2023-12-14 12:38:52 +0530

    chore(project): project setup

---
+++ AlertKit-iOS/AlertKit_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertKit_iOSApp.swift
+//  AlertKit-iOS
+//
+//  Created by Krati Mittal on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AlertKit_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlertKit-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlertKit-iOS
+//
+//  Created by Krati Mittal on 14/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
