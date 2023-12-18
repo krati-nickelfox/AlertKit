10f95fe93e069cd82ca8c6adeeb4c2f0248493ee
shagun-nickelfox <shagun.bansal@nickelfox.com>
2023-12-18 16:21:57 +0530

    feat(CustomAlertView): seggregate the different views and change the naming of variables

---
+++ AlertKit-iOS/Views/CustomAlertView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomAlertView.swift
+//  AlertKit-iOS
+//
+//  Created by Nickelfox on 15/12/23.
+//
+
+import SwiftUI
+
+struct CustomAlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomAlertView()
+}
